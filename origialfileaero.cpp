61a28fdb4be250d6c839aad20eb3e9e8b64ff1bd
jameshackman3 <122382316+jameshackman3@users.noreply.github.com>
2023-02-27 22:07:42 +0530

    Add files via upload

---
+++ origialfileaero.cpp
@@ -0,0 +1,419 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+void mainMenu();
+void flights();
+    
+    
+    int main()
+    {
+        flights();
+        
+    }
+    void flights(){
+    int choiceboard,choicedest,moneyadd=0;
+    int check1,check2,back,charges,choiceflight;
+
+        do{
+                check1=0;
+            cout<<"Welcome to the Airlines!"<<endl;
+        cout<<"Enter your Boarding destination"<<endl;
+        string flight[] = {"Dubai","Canada","UK","USA","Australia","Europe"};
+        for (int i = 0; i < 6; i++)
+        {
+            cout<<(i+1)<<". Flight from "<<flight[i]<<endl;
+        }
+        cout<<endl;
+        
+        cin>>choiceboard;
+        switch(choiceboard)
+        {
+            case 1:moneyadd=4000;
+                    break;
+
+            case 2:moneyadd=8000;
+                    break;      
+
+            case 3:moneyadd=9000;
+                    break;  
+            case 4:moneyadd=6000;
+                    break;
+
+            case 5:moneyadd=8000;
+                    break;        
+            case 6:moneyadd=7500;
+                    break;
+
+            default:cout<<"*********************Invalid Selection************************"<<endl<<endl<<endl;
+                            check1=1;
+                    break;     
+        }
+
+    }while(check1==1);
+
+       do{
+        check2=0;
+                   cout<<"Destination "<<endl;
+         string flight[] = {"Dubai","Canada","UK","USA","Australia","Europe"};
+         for (int i = 0; i < 6; i++)
+         {
+             cout<<(i+1)<<". Flight To "<<flight[i]<<endl;
+         }
+         cout<<"Press the number To Enter your Destination : ";
+         cin>>choicedest;
+         cout<<endl;
+         if(choiceboard==choicedest){
+                cout<<"Boarding Station and the Destinatiion Cannot be Same"<<endl;
+                check2=1;
+              continue;
+        }
+        cout<<"Your Entered destination -"<< flight[choicedest];
+        }while(check2==1);
+
+        switch(choicedest)
+        {
+
+        
+        case 1:
+            cout<<"Welcome to Dubai Emirates"<<endl;
+            cout<<"Enjoy the journey!"<<endl;
+            cout<<endl;
+            cout<<"following are the flights"<<endl;
+            cout<<"1) DUB - 454"<<endl;
+            cout<<"\t06-07-2022  8:00AM  10hrs  Rs. "<<14000+moneyadd<<endl;
+            cout<<"2) DUB - 987"<<endl;
+            cout<<"\t08-07-2022  4:00AM  9hrs  Rs."<<16000+moneyadd<<endl;
+            cout<<"3) DUB - 326"<<endl;
+            cout<<"\t09-07-2022  8:00AM  10hrs  Rs. "<<moneyadd+140000<<endl;
+            cout<<"4) DUB - 126"<<endl;
+            cout<<"\t11-07-2022  5:00PM  10hrs  Rs."<<moneyadd+10000<<endl;
+            cout<<"Select the flight that you want to pick: "<<endl;
+            cin>>choiceflight;
+            if (choiceflight==1)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight DUB - 454."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==2)
+            {
+                charges = 16000+moneyadd;
+                cout<<"You have succesfully books the flight DUB - 987."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==3)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight DUB - 326."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==4)
+            {
+                charges = 10000+moneyadd;
+                cout<<"You have succesfully books the flight DUB - 126."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else{
+                cout<<"Inavlid input, shifting to the previous menu.";
+                flights();
+            }
+            cout<<"Press 0 to go back to the main menu."<<endl;
+            cin>>back;
+            if(back==0){
+                mainMenu();
+            }
+            else
+            {
+                mainMenu();
+            }
+            
+            break;
+            case 2:
+            cout<<"Welcome to Canadian Airlines"<<endl;
+            cout<<"Enjoy the journey!"<<endl;
+            cout<<endl;
+            cout<<"following are the flights"<<endl;
+            cout<<"1) CA - 454"<<endl;
+            cout<<"\t06-07-2022  8:00AM  10hrs  Rs. "<<14000+moneyadd<<endl;
+            cout<<"2) CA - 987"<<endl;
+            cout<<"\t08-07-2022  4:00AM  9hrs  Rs."<< 16000+moneyadd<<endl;
+            cout<<"3) CA - 326"<<endl;
+            cout<<"\t09-07-2022  8:00AM  10hrs  Rs."<< 14000+moneyadd<<endl;
+            cout<<"4) CA - 126"<<endl;
+            cout<<"\t11-07-2022  5:00PM  10hrs  Rs."<<15700+moneyadd<<endl;
+            cout<<"Select the flight that you want to pick: "<<endl;
+            cin>>choiceflight;
+            if (choiceflight==1)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight CA - 454."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==2)
+            {
+                charges = 16000+moneyadd;
+                cout<<"You have succesfully books the flight CA - 987."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==3)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight CA - 326."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==4)
+            {
+                charges = 10000+moneyadd;
+                cout<<"You have succesfully books the flight CA - 126."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else{
+                cout<<"Inavlid input, shifting to the previous menu.";
+                flights();
+            }
+            cout<<"Press 0 to go back to the main menu."<<endl;
+            cin>>back;
+            if(back==0)
+            {
+                mainMenu();
+            }
+            else
+            {
+                mainMenu();
+            }
+            break;
+        
+        case 3:
+            cout<<"Welcome to United Kingdom Airlines"<<endl;
+            cout<<"Enjoy the journey!"<<endl;
+            cout<<endl;
+            cout<<"following are the flights"<<endl;
+            cout<<"1) UK - 454"<<endl;
+            cout<<"\t06-07-2022  8:00AM  10hrs  Rs." <<moneyadd+14000<<endl;
+            cout<<"2) UK - 987"<<endl;
+            cout<<"\t08-07-2022  4:00AM  9hrs  Rs." <<16000+moneyadd<<endl;
+            cout<<"3) UK - 326"<<endl;
+            cout<<"\t09-07-2022  8:00AM  10hrs  Rs."<< 14000+moneyadd<<endl;
+            cout<<"4) UK - 126"<<endl;
+            cout<<"\t11-07-2022  5:00PM  10hrs  Rs." <<10000+moneyadd<<endl;
+            cout<<"Select the flight that you want to pick: "<<endl;
+            cin>>choiceflight;
+            if (choiceflight==1)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight UK - 454."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==2)
+            {
+                charges = 16000+moneyadd;
+                cout<<"You have succesfully books the flight UK - 987."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==3)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight UK - 326."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==4)
+            {
+                charges = 10000+moneyadd;
+                cout<<"You have succesfully books the flight UK - 126."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else{
+                cout<<"Inavlid input, shifting to the previous menu.";
+                flights();
+            }
+            cout<<"Press 0 to go back to the main menu."<<endl;
+            cin>>back;
+            if(back==0)
+            {
+                mainMenu();
+            }
+            else
+            {
+                mainMenu();
+            }
+            break;
+            
+        case 4:
+            cout<<"Welcome to USA Airlines"<<endl;
+            cout<<"Enjoy the journey!"<<endl;
+            cout<<endl;
+            cout<<"following are the flights"<<endl;
+            cout<<"1) USA - 454"<<endl;
+            cout<<"\t06-07-2022  8:00AM  10hrs  Rs."<< moneyadd+14000<<endl;
+            cout<<"2) USA - 987"<<endl;
+            cout<<"\t08-07-2022  4:00AM  9hrs  Rs. "<<16000+moneyadd<<endl;
+            cout<<"3) USA - 326"<<endl;
+            cout<<"\t09-07-2022  8:00AM  10hrs  Rs." <<14000+moneyadd<<endl;
+            cout<<"4) USA - 126"<<endl;
+            cout<<"\t11-07-2022  5:00PM  10hrs  Rs. "<<10000+moneyadd<<endl;
+            cout<<"Select the flight that you want to pick: "<<endl;
+            cin>>choiceflight;
+            if (choiceflight==1)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight USA - 454."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==2)
+            {
+                charges = 16000+moneyadd;
+                cout<<"You have succesfully books the flight USA - 987."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==3)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight USA - 326."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==4)
+            {
+                charges = 10000+moneyadd;
+                cout<<"You have succesfully books the flight USA - 126."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else{
+                cout<<"Inavlid input, shifting to the previous menu.";
+                flights();
+            }
+            cout<<"Press 0 to go back to the main menu."<<endl;
+            cin>>back;
+            if(back==0)
+            {
+                mainMenu();
+            }
+            else
+            {
+                mainMenu();
+            }
+            break;
+        case 5:
+            cout<<"Welcome to Australian Airlines"<<endl;
+            cout<<"Enjoy the journey!"<<endl;
+            cout<<endl;
+            cout<<"following are the flights"<<endl;
+            cout<<"1) AS - 454"<<endl;
+            cout<<"\t06-07-2022  8:00AM  10hrs  Rs."<< 14000+moneyadd<<endl;
+            cout<<"2) AS - 987"<<endl;
+            cout<<"\t08-07-2022  4:00AM  9hrs  Rs. "<<16000+moneyadd<<endl;
+            cout<<"3) AS - 326"<<endl;
+            cout<<"\t09-07-2022  8:00AM  10hrs  Rs." <<14000+moneyadd<<endl;
+            cout<<"4) AS - 126"<<endl;
+            cout<<"\t11-07-2022  5:00PM  10hrs  Rs." <<10000+moneyadd<<endl;
+            cout<<"Select the flight that you want to pick: "<<endl;
+            cin>>choiceflight;
+            if (choiceflight==1)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight AS - 454."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==2)
+            {
+                charges = 16000+moneyadd;
+                cout<<"You have succesfully books the flight AS - 987."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==3)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight AS - 326."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==4)
+            {
+                charges = 10000+moneyadd;
+                cout<<"You have succesfully books the flight AS - 126."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else{
+                cout<<"Invalid input, shifting to the previous menu.";
+                flights();
+            }
+            cout<<"Press 0 to go back to the main menu."<<endl;
+            cin>>back;
+            if(back==0)
+            {
+                mainMenu();
+            }
+            else
+            {
+                mainMenu();
+            }
+            break;
+            case 6:
+            cout<<"Welcome to Europian Airlines"<<endl;
+            cout<<"Enjoy the journey!"<<endl;
+            cout<<endl;
+            cout<<"following are the flights"<<endl;
+            cout<<"1) EU - 454"<<endl;
+            cout<<"\t06-07-2022  8:00AM  10hrs  Rs."<< 14000+moneyadd<<endl;
+            cout<<"2) EU - 987"<<endl;
+            cout<<"\t08-07-2022  4:00AM  9hrs  Rs. "<<16000+moneyadd<<endl;
+            cout<<"3) EU - 326"<<endl;
+            cout<<"\t09-07-2022  8:00AM  10hrs  Rs." <<14000+moneyadd<<endl;
+            cout<<"4) EU - 126"<<endl;
+            cout<<"\t11-07-2022  5:00PM  10hrs  Rs. "<<10000+moneyadd<<endl;
+            cout<<"Select the flight that you want to pick: "<<endl;
+            cin>>choiceflight;
+            if (choiceflight==1)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight EU - 454."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==2)
+            {
+                charges = 16000+moneyadd;
+                cout<<"You have succesfully books the flight EU - 987."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==3)
+            {
+                charges = 14000+moneyadd;
+                cout<<"You have succesfully books the flight EU - 326."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }else if (choiceflight==4)
+            {
+                charges = 10000+moneyadd;
+                cout<<"You have succesfully books the flight EU - 126."<<endl;
+                cout<<"You can go back to menu and get the ticket!"<<endl;
+                cout<<charges;
+            }
+            else{
+                cout<<"Invalid input, shifting to the previous menu.";
+                flights();
+            }
+            cout<<"Press 0 to go back to the main menu."<<endl;
+            cin>>back;
+            if(back==0)
+            {
+                mainMenu();
+            }
+            else
+            {
+                mainMenu();
+            }
+            break;
+            
+        default:
+            cout<<"Invalid input, shifting to the main menu.";
+            mainMenu();
+            break;
+        }
+        
+       
+        
+        }   
+    
+    void mainMenu()
+    {
+        cout<<"You got successfully exited"<<endl;
+    }
---
+++ registrationwe3.cpp
@@ -0,0 +1,219 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+#include<fstream>
+using namespace std;
+class newuser
+{
+    private:
+    string Name;
+    int age;
+    string phonenumber,Nationality,Passport,username,password;
+    char gender;
+    public:
+   newuser(string Name,int age,string phonenumber,char gender,string Nationality,string Passport,string username,string password)
+    {
+        this->Name=Name;
+        this->age=age;
+        this->phonenumber=phonenumber;
+        this->gender=gender;
+        this->Nationality=Nationality;
+        this->Passport=Passport;
+        this->username=username;
+        this->password=password;
+
+    }
+   /* void Namereg(string Name)
+    {
+        this->Name=Name;
+    }
+    void agereg(int age)
+    {
+        this->age;
+    }
+    void genderreg(char gender)
+    {
+        this->gender=gender;
+    }
+    void phonrnoreg(string phonenumber)
+    {
+        this->phonenumber=phonenumber;
+    }
+    void nationalityreg(string Nationality)
+    {
+        this->Nationality=Nationality;
+    }*/
+    void 
+    display()
+    {
+        cout<<"Your name is "<<Name<<endl;
+        cout<<"Your age is "<<age<<endl;
+        cout<<"Your Phone number"<<phonenumber<<endl;
+        cout<<"Your Gender is "<<gender<<endl;
+        cout<<"Your Nationality is"<<Nationality<<endl;
+        cout<<"Passport Number is"<<Passport<<endl;
+        cout<<"Data Stored Successfull"<<endl;
+        cout<<"For Safety Reasons the username and password are not Displayed"<<endl;
+    }
+    void inputtofileusername()//to write the input username in the usernamefile
+{
+    ofstream usernamefile;
+    usernamefile.open("Username.txt");
+    usernamefile<<username;
+    usernamefile.close();
+}
+void inputtofilepassword()
+{
+    ofstream passwordfile;
+    passwordfile.open("userpassword.txt");
+    passwordfile<<password;
+    passwordfile.close();
+}
+void outputfromfileusername()//to print the data from the usernamefile
+{
+    string line;
+    ifstream usernamefile;
+    usernamefile.open("Username.txt",ios::out);
+    usernamefile>>line;
+    cout<<"The enetered username";
+    cout<<line<<endl;
+    usernamefile.close();
+}
+void outputfromthefilepassword()
+{
+    string line;
+    ifstream passwordfile;
+    passwordfile.open("userpassword.txt");
+    passwordfile>>line;
+    cout<<"The Entered password";
+    cout<<line<<endl;
+    passwordfile.close();
+}
+
+};
+string  phonenumber();
+string Nationality();
+char gender();
+string Passportnumber();
+string Username();
+string Password();
+int main()
+{
+    string namereg,phonenumberreg,f_phonenumberreg,nationality,passportnumber,username,password;
+    int agereg;
+    int genderreg;
+    
+    cout<<"Enter Your Name "<<endl;
+    getline(cin,namereg);
+    cout<<"Enter your Age "<<endl;
+    cin>>agereg; 
+    phonenumberreg=phonenumber();
+    genderreg=gender();
+    cout<<"Phase Two registrtation"<<endl;
+    passportnumber=Passportnumber();
+    nationality=Nationality();
+    username=Username();
+    password=Password();
+    newuser user(namereg,agereg,phonenumberreg,genderreg,nationality,passportnumber,username,password);
+    user.display();
+    user.inputtofileusername();
+    user.outputfromfileusername();
+    user.inputtofilepassword();
+    user.outputfromthefilepassword();
+    return 0;
+}
+string Passportnumber()
+{
+    string passportnumber;
+    cout<<"Enter the Passport Number"<<endl;
+    cin>>passportnumber;
+    cout<<"hi";
+    return passportnumber;
+
+}
+string Username()
+{
+    string username;
+    cout<<"Enter your customized username"<<endl;
+    cin>>username;
+    return username;
+
+}
+string Password()
+{
+    string password;
+    cout<<"Enter the Password "<<endl;
+    cin>>password;
+    return password;
+}
+string Nationality()//nationality function
+
+ {
+    string nationality;
+    cout<<"Enter Your Nationality"<<endl;
+    cin>>nationality;
+    cout<<"hi"<<endl;
+    return nationality;
+
+ }
+
+char gender()
+{
+    char genderreg;
+    
+    //convert to check the gender 
+    do
+    {
+        cout<<"Enter your Gender"<<endl;
+        cout<<"Press M for Male \n Press F for female \n Press N for third Gender"<<endl;
+       cin>>genderreg;
+        switch(genderreg)
+    {
+        case 'm':return 'M';
+                break;
+        case 'f':return 'F';
+                break;
+        case 'n':return 'N';
+                break;
+        default: cin.ignore(1000,'\n');
+        cout<<"ENTER AGAIN"<<endl;
+    }
+    } while (1);
+    
+    
+    return 'x';
+    
+    
+}
+bool check(string phone){//to check phonenumber function
+    if(phone.length()!=10){
+        return false;
+    }
+    for(int i=0;i<phone.length();i++)
+    {
+    if((int)phone[i]-48>=0 && (int)phone[i]-48<=9)
+    {
+      continue;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return true;
+}
+string phonenumber(){
+    string phone_number;
+    cout<<"Enter your phone number: ";
+    cin>>phone_number;
+    while(!check(phone_number)){
+        cout<<"You've enetered wrong number\nEnter your phone number: ";
+        cin>>phone_number;
+    }
+    cout<<"You've entered right number"<<endl;
+    return phone_number;
+  
+  
+}
+
+
